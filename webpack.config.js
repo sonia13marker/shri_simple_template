44d2862b2d967d3ec6057b3ff97412b722db8208
sonia13marker <soniamarker13@gmail.com>
2022-10-27 13:26:12 +0300

    исправления 2.0
    уже с добавлением большей инфы, вроде как работающим сервером и тд.

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,32 @@ const config = {
         test: /\, (ts|tsx)$/i,
         loader: 'ts-loader',
         exclude: ['/node_modules/'],
+
+
             // @TODO js rule
+
+            {
+        test: /\.m?jsx?$/i,
+        exclude: /node_modules/,
+        use: babelLoader
+      },
+
+
+
             // @TODO css rule
+            //написано также перед этим: file.js    import css from "file.css"; 
+            test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+            
+
+
+            //еще одна попытка загрузить сss loader
+            loader: 'css-loader',
+            options: {
+                modules: true
+            }
+
+
            
             }
         ],
@@ -41,10 +65,35 @@ const config = {
         extensions: ['.tsx', '.ts','.js'],
 
     }
-    // @TODO optimizations
+
+
+
+  // @TODO optimizations
+    optimization: {
+        minimize: true,
+        moduleIds: 'deterministic',
+        innerGraph: true, 
+        concatenateModules: true,
+
+    //(возможно относится к @TODO chunk for runtime)
+        splitChunks: {
+            minChunks: 2,
+            chunks: 'all',
+            minSize: 0,
+        }
+
+  // создание распределенного файла времени выполнения (runtime) (возможно относится к @TODO chunk for runtime)
+  runtimeChunk: 'single',
+
+  }
+
+
     // @TODO lodash treeshaking
+
+
     // @TODO chunk for lodash
     // @TODO chunk for runtime
+  
     // @TODO fallback for crypto
 };
 
